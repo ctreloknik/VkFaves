c38199ab2f26caf9604e1ada9f887181b73d2910
Valery Romanov <ikrakozabra@gmail.com>
2017-11-26 20:53:40 +0300

    Тотальная чистка проекта

--- src/main/java/net/vkfave/repositories/UserRepository.java
+++ src/main/java/net/vkfave/repositories/UserRepository.java
@@ -1,14 +1,8 @@
 package net.vkfave.repositories;
 
-import net.vkfave.dao.UserDao;
-
+import net.vkfave.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.PagingAndSortingRepository;
 
-/**
- * DAO-репозиторий, содержащий базовые CRUD-операции для записей {@code UserDao}.
- * @see PagingAndSortingRepository
- */
-public interface UserRepository extends JpaRepository<UserDao, Long> {
-    UserDao findById(Long id);
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findById(Long id);
 }
