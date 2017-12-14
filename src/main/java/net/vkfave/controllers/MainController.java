2e802487b1c1c3330eb2df0d9029fdee8c8d36bd
Valery Romanov <ikrakozabra@gmail.com>
2017-12-14 13:47:52 +0300

    :japanese_goblin: Добавлен маппер запросов для главной страницы, сама главная страница и API-метод для системной инфы

---
+++ src/main/java/net/vkfave/controllers/MainController.java
@@ -0,0 +1,52 @@
+package net.vkfave.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Контроллер, отвечающий за маппинг запросов для основных страниц приложения
+ */
+@Controller
+public class MainController {
+
+    /**
+     * Устанавливаем файл resources/views/index.ftl в качестве главной страницы
+     */
+    @RequestMapping("/")
+    public String indexPageRequestMapper(ModelMap modelMap) {
+        return "index";
+    }
+
+    /**
+     * Возвращает JSON с информацией о системе. Также нужен для проверки работоспособности API
+     */
+    @GetMapping("/sys/info")
+    @ResponseBody
+    public SystemInfoDO getSystemInfo() {
+        return SystemInfoDO.INSTANCE;
+    }
+
+    private static class SystemInfoDO {
+        public static final SystemInfoDO INSTANCE = new SystemInfoDO(
+                Arrays.asList("Чеботарёв Н.С.", "Романов В.М"),
+                "0.0.1",
+                "https://github.com/ctreloknik/VkFaves"
+        );
+
+        public final List<String> authors;
+        public final String version;
+        public final String repository;
+
+        public SystemInfoDO(List<String> authors, String version, String repository) {
+            this.authors = authors;
+            this.version = version;
+            this.repository = repository;
+        }
+    }
+}
