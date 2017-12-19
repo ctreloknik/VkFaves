fb18e8c8cd1802bccef4f342c199bab0c1250f50
Valery Romanov <ikrakozabra@gmail.com>
2017-12-20 01:19:46 +0300

    Добавлен роут "/auth" на клиент

--- frontend/src/components/routes/auth/Auth.jsx
+++ frontend/src/components/routes/auth/Auth.jsx
@@ -4,24 +4,35 @@ import store from '../../../store'
 import { VK_TOKEN_RECEIVE } from "../../../reducers/vk-auth/actions";
 
 class Auth extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      token: null
+    }
+  }
+
   componentDidMount() {
     const parts = window.location.hash.split("&")
-    const token = (parts.find(part => /access_token/.test(part)) || '').split("=")[0]
-    const userId = (parts.find(part => /user_id/.test(part)) || '').split("=")[0]
+    const token = (parts.find(part => /access_token/.test(part)) || '').split("=")[1]
+    const userId = (parts.find(part => /user_id/.test(part)) || '').split("=")[1]
     store.dispatch({ type: VK_TOKEN_RECEIVE, payload: { token, userId } })
   }
 
+  componentWillReceiveProps = nextProps => {
+    console.log(nextProps)
+    this.setState({ token: nextProps.userData.token })
+  }
 
   render() {
     return (
       <div className="dashboard">
-        Пожалуйста, подождите...
-        Ваш токен: {this.props.userData.token}
+        <p>Пожалуйста, подождите...</p>
+        <p>Токен пользователя: { this.state.token }</p>
       </div>
     )
   }
 }
 
 export default connect(state => ({
-  userData: state.vkUserData.userData,
+  userData: state.vkUserData,
 }))(Auth)
