fb9e0809418f2c8c09b53e1bcfdf6c75a5950e23
Valery Romanov <ikrakozabra@gmail.com>
2017-12-24 23:50:35 +0300

    Эксперименты с cookie

--- src/main/java/net/vkfave/controllers/MainController.java
+++ src/main/java/net/vkfave/controllers/MainController.java
@@ -3,11 +3,11 @@ package net.vkfave.controllers;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.CookieValue;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.Arrays;
 import java.util.List;
 
@@ -24,12 +24,19 @@ public class MainController {
      * Устанавливаем файл resources/views/index.ftl в качестве главной страницы
      */
     @RequestMapping("/")
-    public String indexPageRequestMapper(@CookieValue(name = "accessToken", defaultValue = "none") String accessToken,
-                                         @CookieValue(name = "vkUserId",    defaultValue = "none") String vkUserId,
-                                         ModelMap modelMap) {
+    public String indexPageRequestMapper(@CookieValue(name = "accessToken", required = false) String accessToken,
+                                         @CookieValue(name = "vkUserId", required = false)    String vkUserId,
+                                         @RequestParam(name = "clr", required = false) String clearFlag,
+                                         ModelMap modelMap, HttpServletRequest req, HttpServletResponse resp) {
+        if (clearFlag != null) {
+            for (Cookie cookie : req.getCookies()) {
+                cookie.setMaxAge(0);
+                resp.addCookie(cookie);
+            }
+        }
         modelMap.addAttribute("vkAppId", vkAppId);
-        modelMap.addAttribute("token", accessToken);
-        modelMap.addAttribute("vkUserId", vkUserId);
+        modelMap.addAttribute("token", accessToken != null ? accessToken : "none");
+        modelMap.addAttribute("vkUserId", vkUserId != null ? vkUserId : "none");
         return "index";
     }
 
